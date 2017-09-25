07a262992c0e34f7d28e7cd54c2973b2152792ab
Dmitry <dmitry_kholodok@mail.ru>
2017-09-25 08:30:13 +0600

    Added the 4th lab -> disposing of managed & unmanaged resources

---
+++ FourthLab/App/App/OSHandle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace App
+{
+    public class OSHandle : IDisposable
+    {
+        private bool disposedValue = false;
+        public IntPtr Handle { get; set; }
+
+        public OSHandle() { }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool CloseHandle(IntPtr handle);
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing) { } // managed resources
+
+                //  unmanaged
+                if (Handle != IntPtr.Zero)
+                {   
+                    if (!CloseHandle(Handle))
+                    {
+                        throw new Exception("Impossible to close the handle!");
+                    }
+                }
+
+                disposedValue = true;
+            }
+        }
+
+         ~OSHandle()
+         {
+            Dispose(false); // just unmanaged resourecs
+         }
+
+        public void Dispose()
+        {
+            Dispose(true); // managed & unmanaged resources
+            GC.SuppressFinalize(this); // cancels the finalizer
+        }
+    }
+}
